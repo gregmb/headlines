636322ecbe327e39a59b8cb0621b46630d3dfaf9
Gregory Brucchieri <gregmb@protonmail.com>
2019-11-01 10:07:56 -0400

    weather add

--- headlines.py
+++ headlines.py
@@ -2,6 +2,9 @@ import feedparser
 from flask import Flask
 from flask import render_template
 from flask import request
+import json
+import requests
+from urllib.parse import urlencode, puote_plus
 
 app = Flask(__name__)
 
@@ -21,8 +24,19 @@ def get_news():
     else:
         publication = query.lower()
     feed = feedparser.parse(RSS_FEEDS[publication])
-    return(render_template("home.html", articles=feed['entries']))
-   
+    weather = get_weather("London,UK")
+    return(render_template("home.html", articles=feed['entries'],weather=weather))
+
+def get_weather(query):
+    api_url = 'http://apiopenweathermap.org/data/2.5/weather'?q={ }&appid
+    params = {'appid' : 'ec152d53a4c74873d56523adabb25269', 'q' : city}}
+    data = urllib2.urlopen(url).read()
+    parsed = json.loads(data)
+    weather = None
+    if parsed.get("weather"):
+        weather = {"description":parsed["weather"][0]["description"],"temperature":parsed["main"]["temp"],"city":parsed["name"]
+                  }
+    return weather
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
