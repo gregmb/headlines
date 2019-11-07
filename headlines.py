993de82d0261c2276a14839b322667ca81f3de92
gregmb <gregmb@protonmail.com>
2019-11-07 03:14:56 -0500

    updates to weather

--- headlines.py
+++ headlines.py
@@ -4,7 +4,6 @@ from flask import render_template
 from flask import request
 import json
 import requests
-import objectpath
 
 
 app = Flask(__name__)
@@ -15,9 +14,7 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'iol': 'http://www.iol.co.za/cmlink/1.640'}
 
 
-
 @app.route("/")
-#@app.route("/<publication>")
 def get_news():
     query = request.args.get("publication")
     if not query or query.lower() not in RSS_FEEDS:
@@ -25,19 +22,32 @@ def get_news():
     else:
         publication = query.lower()
     feed = feedparser.parse(RSS_FEEDS[publication])
-    weather = get_weather("London,UK")
-    return(render_template("home.html", articles=feed['entries'],weather=weather))
+    weather = get_weather("London,GB")
+    return(render_template("home.html", articles=feed['entries'],
+                           weather=weather))
 
-def get_weather(city):
-    api_url = 'http://apiopenweathermap.org/data/2.5/weather'
-    params = {'appid' : 'ec152d53a4c74873d56523adabb25269', 'q' : city}
-    data = 
+
+def get_weather(query):
+    api_url = 'http://api.openweathermap.org/data/2.5/weather'
+    city = city_id(query)
+    params = {'appid': 'ec152d53a4c74873d56523adabb25269', 'q': city}
+    data = requests.get(api_url, params=params)
     parsed = json.loads(data)
     weather = None
-    if parsed.get("weather"):
-        weather = {"description":parsed["weather"][0]["description"],"temperature":parsed["main"]["temp"],"city":parsed["name"]
-                  }
+    if parsed["weather"]:
+        weather = {"description": parsed["weather"][0]["description"],
+                   "temperature": parsed["main"]["temp"], "city": parsed["name"]
+                   }
     return weather
+                        
 
+def city_id(input):
+    with open('city.list.json') as f:
+        cityCodes = json.load(f)
+    name, country = input.split(',')
+    code = [x['id'] for x in cityCodes if x['name']==name and x['country']==country]
+    return(code[0])
+   
+   
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
